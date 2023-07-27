2557e836171eb7e6fc6cf822f374efeb3ff43a7e
Artem <101128411+artemKhlv@users.noreply.github.com>
2023-07-27 18:33:08 +0300

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,46 @@
+import openai
+import streamlit as st
+from st_pages import Page, show_pages
+show_pages(
+    [
+        # Page("main.py", "Home", "🏠"),
+        Page("molecule_examine.py", "Molecule Examine", "🧫"),
+        Page("chat.py", "ChatAI", "🪬"),
+        Page("about_us.py", "About us", "🧑🏻‍🔬")
+    ]
+)
+
+st.title("AI Science Chat")
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Задай вопрос, а я постараюсь на него ответить)"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
